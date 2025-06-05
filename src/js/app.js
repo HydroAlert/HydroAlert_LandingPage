ff638fbda7990ea5305d36c2e054b1730372a5bb
Otavio <inabaotavio7@gmail.com>
2025-06-05 19:32:51 -0300

    FEAT: Update footer with social media links and improve layout; enhance mobile menu functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,27 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 mobileMenu.addEventListener("click", mobileNavbar);
 
-
 function mobileNavbar() {
   const navList = document.querySelector(".nav-list");
   const navLinks = document.querySelectorAll(".nav-list li");
   const activeClass = "active";
-  navLinks.forEach((link, index) => {
-    link.style.animation
-    ? (link.style.animation = "")
-    : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-      index / 7 + 0.3
-    }s`);
-  });
-  
+
   navList.classList.toggle(activeClass);
   mobileMenu.classList.toggle(activeClass);
+
+  navLinks.forEach((link, index) => {
+    link.style.animation
+      ? (link.style.animation = "")
+      : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`);
+  });
 }
 
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval(function() {
+setInterval(function () {
   nextImage();
 }, 3000);
 
