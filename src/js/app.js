6a947fb87da75f3eb7d9f45a6ce2208b8c6fc66f
Otavio <inabaotavio7@gmail.com>
2025-06-05 14:10:25 -0300

    FEAT: Implement image slider with navigation; update styles for slider layout and responsiveness

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,34 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 mobileMenu.addEventListener("click", mobileNavbar);
 
+
 function mobileNavbar() {
   const navList = document.querySelector(".nav-list");
   const navLinks = document.querySelectorAll(".nav-list li");
   const activeClass = "active";
   navLinks.forEach((link, index) => {
     link.style.animation
-      ? (link.style.animation = "")
-      : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.3
-        }s`);
+    ? (link.style.animation = "")
+    : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+      index / 7 + 0.3
+    }s`);
   });
-
+  
   navList.classList.toggle(activeClass);
   mobileMenu.classList.toggle(activeClass);
+}
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function() {
+  nextImage();
+}, 3000);
+
+function nextImage() {
+  count++;
+  if (count > 3) {
+    count = 1;
+  }
+  document.getElementById("radio" + count).checked = true;
 }
